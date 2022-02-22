9e9683dea47e8e163891dc4cf750c09a3649f756
Pavel <49401645+PentiumMMX200@users.noreply.github.com>
2022-02-22 19:48:39 +0300

    rebuild cookie system
    Fix

--- electronic journal/auth.php
+++ electronic journal/auth.php
@@ -1,24 +1,21 @@
 <?php
 include 'DBCon.php';
 
-$login=filter_var(trim($_POST['login']),FILTER_SANITIZE_STRING);
-$passwd=filter_var(trim($_POST['password']),FILTER_SANITIZE_STRING);
+$login = filter_var(trim($_POST['login']), FILTER_SANITIZE_STRING);
+$passwd = filter_var(trim($_POST['password']), FILTER_SANITIZE_STRING);
 
-$passwd = hash('ripemd160',$passwd);
+$passwd = hash('ripemd160', $passwd);
 
 $result = $db->prepare("SELECT * FROM `login` WHERE `login`=:logs AND `passwd`=:pass");
-$result->execute(array(":logs"=>$login,":pass"=>$passwd));
+$result->execute(array(":logs" => $login, ":pass" => $passwd));
 
-$user=$result->fetch(PDO::FETCH_ASSOC);
+$user = $result->fetch(PDO::FETCH_ASSOC);
 
-if($user == 0){
-    echo "Такого пользователя не существует";
-    exit();
+if ($user) {
+    $cookie = hash('ripemd160', $user['name'] . 'dfg324');
+    setcookie('log', $cookie, time() + 60 * 60 * 24 * 365, "/");
+
+    header('Location: main.html');
+} else {
+    header('Location: index.php?Invaliddata');
 }
-
-
-$cookie = hash('ripemd160', $user['name'].'dfg324');
-setcookie('log', $cookie, time()+ 60*60*24*365, "/");
-
-header('Location: main.html');
-?>
